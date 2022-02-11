58c2e11b1b8f48912d94f4f5a096f7332ad2b5c6
Monomanya <katyara06@yandex.ru>
2022-02-11 23:38:38 +0300

    homework1

---
+++ hw1.py
@@ -0,0 +1,16 @@
+# задача 1
+
+a = 23
+b = 16
+print(a)
+print(a+b)
+
+print('hello')
+print('hello'+' how are you?')
+
+
+name = input('name:')
+height =int(input('height:'))
+weight = int(input('weight:'))
+print('name:', name, 'height:', height, 'weight', weight)
+print(weight/height)
