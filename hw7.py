5cbbdaa67ef9217de34fc61f5c4940903d4d19d3
Monomanya <katyara06@yandex.ru>
2022-02-13 12:12:24 +0300

    homework7

---
+++ hw7.py
@@ -0,0 +1,11 @@
+# задача 7
+a = float(input('первоначальный результат, км:'))
+b = float(input('желаемый результат, км:'))
+count = 0
+while a <= b:
+    a = a * 1.1
+    count = count + 1
+    print(count)
+
+
+
