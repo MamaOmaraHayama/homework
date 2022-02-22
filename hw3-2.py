7bcf2610e22243fb21d2d085cfb5451f4a8197fc
Monomanya <katyara06@yandex.ru>
2022-02-22 20:36:57 +0300

    homework_lesson3

---
+++ hw3-2.py
@@ -0,0 +1,8 @@
+def user_data(name, last_name, age, city, email, phone_number):
+    print(name, last_name, age, city, email, phone_number)
+
+
+user_data(name='Katya', last_name='Belskaya', age='23', city='Minsk', email='katyara06@yandex.ru',
+          phone_number='2584979')
+
+
